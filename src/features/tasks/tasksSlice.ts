475337bd1e56feacb7fb6871c629b4a10ebcb8c9
Ex1s9 <sadovskyroma@gmail.com>
2024-12-22 13:26:20 +0300

    first commit

---
+++ src/features/tasks/tasksSlice.ts
@@ -0,0 +1,46 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+
+interface Task {
+   id: string;
+   title: string;
+   description: string;
+   completed: boolean;
+}
+
+interface TasksState {
+   tasks: Task[];
+}
+
+const initialState: TasksState = {
+   tasks: [],
+};
+
+const tasksSlice = createSlice({
+   name: "tasks",
+   initialState,
+   reducers: {
+      taskAdded(state, action: PayloadAction<Task>) {
+         state.tasks.push(action.payload);
+      },
+      taskToggled(state, action: PayloadAction<string>) {
+         const task = state.tasks.find((task) => task.id === action.payload);
+         if (task) {
+            task.completed = !task.completed;
+         }
+      },
+      deleteTask(state, action: PayloadAction<string>) {
+         state.tasks = state.tasks.filter((task) => task.id !== action.payload);
+      },
+      updateTask(state, action: PayloadAction<Task>) {
+         const task = state.tasks.find((task) => task.id === action.payload.id);
+         if (task) {
+            task.title = action.payload.title;
+            task.description = action.payload.description;
+         }
+      }
+   },
+});
+
+export const { taskAdded, taskToggled, deleteTask, updateTask } = tasksSlice.actions;
+export default tasksSlice.reducer;
